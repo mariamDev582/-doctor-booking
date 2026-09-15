276fda56580e201f7745c7ca9f10a17958ffb183
Mariam <mariam.212006sara@gmail.com>
2026-09-15 04:28:43 +0200

    fix: update API baseURL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://my-json-server.typicode.com/mariamDev582/-doctor-booking",
   headers: { "Content-Type": "application/json" },
 });
 
